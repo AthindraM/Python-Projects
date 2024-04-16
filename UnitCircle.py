c6be2531c61ebb9d7a446c5e18455949ea4fa58c
AthindraM <athindra.nm4@gmail.com>
2024-04-15 20:57:08 -0400

    started code for UnitCircle.py; created the intro screen and gave the user options (quiz or solver)

--- UnitCircle.py
+++ UnitCircle.py
@@ -1,4 +1,18 @@
 # Started on 04.14.2024
 # Completed on 
 
-# This program is meant to quiz and solve trig nquestions that appear on the unit circle that are common in classes like calculus
+# This program is meant to quiz and solve trig questions that appear on the unit circle that are common in classes like calculus
+
+program_running = True
+
+print("Welcome to my Unit Circle Program!")
+mode = input("What mode would you like to use?\n\tquiz\n\tsolver (COMING SOON)\n")
+
+if mode == "quiz":
+    print("Quiz Mode")
+elif mode == "solver":
+    print("COMING SOON")
+else:
+    print("invalid input")
+
+
